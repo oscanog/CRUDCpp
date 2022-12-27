e731b8bd59eade681f94135cf13ef362f1b8b9cc
oscanog <oscar.nogoy08@gmail.com>
2022-12-27 16:46:34 +0700

    option for transaction display added

--- CRUDcpp/CRUDcpp.cpp
+++ CRUDcpp/CRUDcpp.cpp
@@ -9,13 +9,15 @@ string path = "C:\\Users\\M SSD Samsung Evo\\Documents\\CC 101\\ALL LEARNINGS\\C
 void main()
 {
     system("CLS");
-    std::cout << "MENU\n";
-    std::cout << "1 Add New Books\n";
-    std::cout << "2 List Books\n";
-    std::cout << "3 Search Books\n";
-    std::cout << "4 Update Books\n";
-    std::cout << "5 Delete Books\n";
-    std::cout << "6 Quit\n";
+    int option;
+    cout << "MENU\n";
+    cout << "1 Add New Books\n";
+    cout << "2 List Books\n";
+    cout << "3 Search Books\n";
+    cout << "4 Update Books\n";
+    cout << "5 Delete Books\n";
+    cout << "6 Quit\n";
+    cout << "Enter Transaction: "; cin >> option;
 
     cin.get();
     cin.get();
