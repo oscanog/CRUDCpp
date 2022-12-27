27cad729d54ac2153fb1027afa7aaf717ae9d297
Melvin Nogoy <m.viner001@gmail.com>
2022-12-27 17:24:57 +0800

    First Commit

---
+++ CRUDcpp/CRUDcpp.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+string path = "C:\\Users\\M SSD Samsung Evo\\Documents\\CC 101\\ALL LEARNINGS\\CRUD\\CRUDVStudio\\CRUDCpp";
+
+
+void main()
+{
+    system("CLS");
+    std::cout << "MENU\n";
+    std::cout << "1 Add New Books\n";
+    std::cout << "2 List Books\n";
+    std::cout << "3 Search Books\n";
+    std::cout << "4 Update Books\n";
+    std::cout << "5 Delete Books\n";
+    std::cout << "6 Quit\n";
+
+    cin.get();
+    cin.get();
+
+}
